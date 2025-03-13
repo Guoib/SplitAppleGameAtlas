c1b73638404ed09967bef641dede9888b1e6feae
Guoyibin <18324332230@163.com>
2025-03-13 17:03:00 +0800

    feat: 增加进度条显示

--- src/utils/SplitTool.ts
+++ src/utils/SplitTool.ts
@@ -1,10 +1,13 @@
 import path from "path";
 import walkSync from "walk-sync";
-import fs from "fs";
+import fs, { readFile, readFileSync, writeFileSync } from "fs";
 import fse from "fs-extra";
 import plist from "plist";
 import images from "images";
 import { Plist } from "../types";
+import { exec } from "child_process";
+import cgbiToPng from "cgbi-to-png";
+import ProgressBar from "progress";
 
 interface SplitToolConstructorProps {
   sourcePath?: string;
@@ -12,6 +15,8 @@ interface SplitToolConstructorProps {
 }
 
 class SplitTool {
+  private count = 0;
+
   private sourcePath = path.join(__dirname, "../../source");
   private outputPath = path.join(__dirname, "../../output");
 
@@ -29,19 +34,44 @@ class SplitTool {
     }
   }
 
-  public split() {
+  public async split() {
     this.checkDir(this.sourcePath);
 
     const plistArray = walkSync(this.sourcePath, { globs: ["**/*.plist"] });
 
-    for (const plist of plistArray) {
-      this.splitPlist(plist);
+    for (let i = 0; i < plistArray.length; i++) {
+      const plist = plistArray[i];
+      console.log(`正在处理第 ${i + 1} 个 plist 文件: ${plist}`);
+      await this.splitPlist(plist);
     }
+
+    console.log(`处理完成，共生成 ${this.count} 个图片`);
   }
 
-  private splitPlist(plistPath: string) {
-    const wholePlistPath = path.join(this.sourcePath, plistPath);
+  private convertBinaryPlistToXml(inputFilePath: string, outputFilePath: string) {
+    return new Promise((resolve, reject) => {
+      exec(`plutil -convert xml1 ${inputFilePath} -o ${outputFilePath}`, (error, stdout, stderr) => {
+        if (error) {
+          reject(`执行 plutil 命令时出错: ${error.message}`);
+          return;
+        }
+        resolve(`plutil 命令执行成功: ${stdout}`);
+      });
+    });
+  }
 
+  private async convertCgBIPngToPng(inputFilePath: string, outputFilePath: string) {
+    const cgbiBuffer = readFileSync(inputFilePath);
+    const pngBuffer = cgbiToPng.revert(cgbiBuffer);
+    writeFileSync(outputFilePath, pngBuffer);
+  }
+
+  private async splitPlist(plistPath: string) {
+    const wholePlistPath = path.join(this.sourcePath, plistPath);
+    const wholePngPath = wholePlistPath.split(".")[0] + ".png";
+
+    await this.convertBinaryPlistToXml(wholePlistPath, wholePlistPath);
+    await this.convertCgBIPngToPng(wholePngPath, wholePngPath);
     const plistFile = fs.readFileSync(wholePlistPath, "utf-8");
     const plistData = plist.parse(plistFile) as Plist;
 
@@ -50,7 +80,15 @@ class SplitTool {
 
     const bigImage = images(path.join(this.sourcePath, dirName + ".png"));
 
+    const total = Object.keys(plistData.frames).length;
+    const progressBar = new ProgressBar("处理文件: :current/:total [:bar] :percent", { total, width: 40 });
+
     for (const key in plistData.frames) {
+      /**
+       * 总数累计
+       */
+      this.count++;
+      progressBar.tick();
       const rectStr = plistData.frames[key].frame.match(/\d+(.\d+)?/g);
       const rectArr = rectStr.join(",").split(",");
 
